cf8e103a986228a483f838b9de24b743c8ac0b59
b-colby <benjamin_colby@student.uml.edu>
2017-02-14 11:32:10 -0500

    terrain init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, movevertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+        }
+    }
+
+
+   
+}
---
+++ Assets/Scripts/Trig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trig : MonoBehaviour {
+
+    public Text goal;
+   
+
+    void Start()
+    {
+        goal.text = "";
+        
+    }
+
+  
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ball"))
+        {
+            goal.text = "GOAL";
+            
+        }
+        else if (other.gameObject.CompareTag("car"))
+        {
+            goal.text = "";
+        }
+    }
+
+   
+ 
+	}
+
